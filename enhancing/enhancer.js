b4867249df1a09df5331b6a4cbb0959f11c10e87
Memitaru <amandatwah@gmail.com>
2019-06-19 22:44:41 -0700

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,10 +6,27 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(item.enhancement < 20){
+    item.enhancement++
+  }
+  return {...item};
 }
 
 function fail(item) {
+
+  
+  
+  if (item.enhancement < 15) {
+    item.durability -= 5
+  } else {
+    item.durability -=10
+  }
+
+  if(item.enhancement > 15){
+    item.enhancement--
+  }
+  
+  
   return { ...item };
 }
 
